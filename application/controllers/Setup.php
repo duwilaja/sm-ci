a77247a5864d2f60cca83cf072397c6d068a0469
teguh sabara <teguhsabara16@gmail.com>
2020-12-14 14:37:33 +0700

    datamaster

--- application/controllers/Setup.php
+++ application/controllers/Setup.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class SetupData extends CI_Controller {
+class Setup extends CI_Controller {
     
     public function __construct()
     {
