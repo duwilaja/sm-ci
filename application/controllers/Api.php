b530689b1d07427c42490c71912c2b1865518dc8
teguh sabara <teguhsabara16@gmail.com>
2020-12-16 13:58:04 +0700

    api

---
+++ application/controllers/Api.php
@@ -0,0 +1,92 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('MLaporan','ml');
+        
+    } 
+
+    //  ini get input type select di form laporan gatur lalin
+    public function get_kegiatan($table,$where)
+    {
+        $status = false;
+        $msg = "Gagal Mendapatkan List Kegiatan";
+        
+        $get = $this->ml->get($table,$where);
+
+        if ($get) {
+            $status = true;
+            $msg = "Berhasil Mendapatkan List Kegiatan";
+        }
+        $dt = [
+			'msg' => $msg,
+			'status' => $status
+		];
+
+		echo json_encode($dt);
+    }
+
+    public function get_kejadian($table,$where)
+    {
+        $status = false;
+        $msg = "Gagal Mendapatkan List Kejadian";
+        
+        $get = $this->ml->get($table,$where);
+
+        if ($get) {
+            $status = true;
+            $msg = "Berhasil Mendapatkan List Kejadian";
+        }
+        $dt = [
+			'msg' => $msg,
+			'status' => $status
+		];
+
+		echo json_encode($dt);
+    }
+
+    public function get_parameter_antrian($table,$where)
+    {
+        $status = false;
+        $msg = "Gagal Mendapatkan List Parameter Antrian";
+        
+        $get = $this->ml->get($table,$where);
+
+        if ($get) {
+            $status = true;
+            $msg = "Berhasil Mendapatkan List Parameter Antrian";
+        }
+        $dt = [
+			'msg' => $msg,
+			'status' => $status
+		];
+
+		echo json_encode($dt);
+    }
+
+    public function get_penyebab($table,$where)
+    {
+        $status = false;
+        $msg = "Gagal Mendapatkan List Penyabab";
+        
+        $get = $this->ml->get($table,$where);
+
+        if ($get) {
+            $status = true;
+            $msg = "Berhasil Mendapatkan List Penyabab";
+        }
+        $dt = [
+			'msg' => $msg,
+			'status' => $status
+		];
+
+		echo json_encode($dt);
+    }
+
+
+
+    // end get input type select di form laporan gatur 
+}
