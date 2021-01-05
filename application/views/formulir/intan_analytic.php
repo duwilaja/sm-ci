84350e436d3bc4f3cdcaced1f206d656b29d4a73
dwil <apaaja.software@gmail.com>
2021-01-05 17:36:32 +0700

    intan+ais

--- application/views/formulir/intan_analytic.php
+++ application/views/formulir/intan_analytic.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); 
 
 $cols="nrp,unit,polda,polres,dinas,subdinas,tgl,dasar,nomor,";
-$cols.="jenis,jalan,lat,lng";
+$cols.="kasus,jalan,lat,lng,callin,finished";
 ?>
 
 <input type="hidden" name="tablename" value="intan_analytic">
@@ -10,11 +10,12 @@ $cols.="jenis,jalan,lat,lng";
 <div class="row">
 	<div class="col-sm-6 col-md-3">
 		<div class="form-group">
-			<label class="form-label">Jenis</label>
-			<select name="jenis" class="form-control" placeholder="" >
-			<option value="Kemacetan">Kemacetan</option>
+			<label class="form-label">Kasus</label>
+			<select name="kasus" class="form-control" placeholder="" >
 			<option value="Kecelakaan">Kecelakaan</option>
-			<!--option value="Kondisi Jalur">Kondisi Jalur</option-->
+			<option value="Kemacetan">Kemacetan</option>
+			<option value="Kontingensi">Kontingensi</option>
+			<option value="Bencana Alam">Bencana Alam</option>
 			</select>
 		</div>
 		<div class="form-group">
@@ -36,9 +37,23 @@ $cols.="jenis,jalan,lat,lng";
 			</div>
 		</div>
 		<div class="row">
+			<div class="col-sm-6 col-md-6">
+				<div class="form-group">
+					<label class="form-label">Call IN</label>
+					<input type="text" name="callin" class="form-control timepicker" placeholder="" >
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-6">
+				<div class="form-group">
+					<label class="form-label">Finished</label>
+					<input type="text" name="finished" class="form-control timepicker" placeholder="" >
+				</div>
+			</div>
+		</div>
+		<div class="row">
 			<div class="col-md-12">
 			  <ul class="list-group">
-				<li class="list-group-item justify-content-between">Faskes <span class="badgetext badge badge-primary bg-green badge-pill"><?php echo count($faskes)?></span></li>
+				<li class="list-group-item justify-content-between">Faskes <span class="badgetext badge badge-success badge-pill"><?php echo count($faskes)?></span></li>
 				<li class="list-group-item justify-content-between">Ambulance <span class="badgetext badge badge-primary bg-orange badge-pill"><?php echo count($ambulance)?></span></li>
 				<li class="list-group-item justify-content-between">Pos Polisi <span class="badgetext badge badge-primary bg-lime badge-pill"><?php echo count($pospol)?></span></li>
 				<li class="list-group-item justify-content-between">Pos PJR <span class="badgetext badge badge-info badge-pill"><?php echo count($pospjr)?></span></li>
@@ -48,7 +63,33 @@ $cols.="jenis,jalan,lat,lng";
 		</div>
 	</div>
 	<div class="col-sm-6 col-md-9">
-		<div id="map" style="width: 100%; height: 450px;"></div>
+		<div id="map" style="width: 100%; height: 500px;"></div>
+		<!--div class="row">
+			<div class="col-12">
+				<div class="card">
+					<div class="card-body table-responsive">
+						<table class="table card-table table-vcenter text-nowrap table-primary">
+							<thead class="bg-primary text-white">
+								<tr>
+									<th style="color:white;">Case</th>
+									<th style="color:white;">Call In</th>
+									<th style="color:white;">Finished</th>
+									<th style="color:white;">Duration</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Kecelakaan</td>
+									<td>10:00</td>
+									<td>11:10</td>
+									<td>1:10</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div-->
 	</div>
 </div>
 
@@ -77,6 +118,12 @@ jvalidate = $("#myf").validate({
 			required : true
 		},
 		"lng" : {
+			required : true
+		},
+		"callin" : {
+			required : true
+		},
+		"finished" : {
 			required : true
 		}
     }});
@@ -143,7 +190,7 @@ function draw_pointers(title,color,icon,data){
 			//var title = a['name'];
 			//var color = a['onoff']>0?"green":"red";
 			if(a['lat'].trim()!=""&&a['lng'].trim()!=""){
-				if(icon==""){
+				if(title=="Koordinasi"){
 					ttl=a["giat"];
 					switch(ttl){
 						case "Pembangunan Jalan" : icn="road";  break;
@@ -153,6 +200,8 @@ function draw_pointers(title,color,icon,data){
 						case "Konser" : icn="child";  break;
 						default : icn = "slideshare"; break;
 					}
+				}else{
+					ttl=title+' '+a['nama'];
 				}
 				var myicon = new L.AwesomeMarkers.icon({icon: icn, prefix: 'fa', markerColor: color});
 				L.marker(new L.LatLng(a['lat'], a['lng']), { title: ttl, icon: myicon }).addTo(map);
