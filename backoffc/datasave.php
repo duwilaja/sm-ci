ebe7aba0eb827b4c7e164229df5d8dde716a42e9
dwil <apaaja.software@gmail.com>
2021-02-23 13:33:18 +0700

    rekap tmc ssc

--- backoffc/datasave.php
+++ backoffc/datasave.php
@@ -199,6 +199,14 @@ if($mn=='tmcmonlalin'){
 	$res=crud($conn);
 	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
 }
+if($mn=='pmacet'){
+	$res=crud($conn);
+	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
+}
+if($mn=='pmacetd'){
+	$res=crud($conn);
+	$code=$res[0]; $ttl=$res[1]; $msgs=$res[2];
+}
 
 
 /*
