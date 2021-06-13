49ed43367064860ee4c4fb48fba5e19a7e73330b
Sahrul Rizal <sahrulrizal22@gmail.com>
2021-06-13 19:25:19 +0700

    test email

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,6 +18,35 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	public function sendmail($to,$sub,$msg){
+		$config = Array(
+			'protocol' => 'smtp',
+			'smtp_host' => 'smtp-pulse.com',
+			'smtp_port' => 465,
+			'smtp_user' => 'matrik.mmt@gmail.com',
+			'smtp_pass' => 'DcNbPQF9PsQ',
+			'smtp_crypto'   => 'ssl',
+			'mailtype'  => 'html', 
+			'charset'   => 'iso-8859-1',
+			'priority'  => 1
+		);
+		$this->load->library('email', $config);
+		$this->email->set_newline("\r\n");
+		
+		$this->email->from('info@matrik.co.id', 'Smart Management App');
+		$this->email->to($to);
+		$this->email->subject("Smart Management : $sub");
+		$this->email->message($msg);
+		
+		return $this->email->send();
+	}
+
+	public function cek_mail()
+	{
+		var_dump($this->sendmail('sahrulrizal22@gmail.com','Test','Gua cuman test aja ya'));
+	}
+
 	public function index()
 	{
 		$data['pangkat'] = comboopts($this->db->select('pang_id as v,pang_nam as t')->get('pangkat')->result());
